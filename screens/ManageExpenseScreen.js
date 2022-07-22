0520a8e618056f94754e71a1e51d500b8272b79f
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-22 12:11:05 +0200

    dynamically set up MES title

--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -1,6 +1,18 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
-export const ManageExpense = () => {
+export const ManageExpense = ({ route, navigation }) => {
+
+  const editexExpenseId = route.params?.expenseId;
+  const isEditing = !!editexExpenseId; // converts value into a boolean
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing? "Edit Expense" : "Add Expense"
+    })
+  }, [navigation, isEditing]);
+
+
   return (
     <Text>ManageExpense</Text>
   );
