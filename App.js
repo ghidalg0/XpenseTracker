1a7adea34acb6b30ce9d01e0b97df0291887c454
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-22 09:56:16 +0200

    nested navigation implemented

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+// Screens
+import { ManageExpense } from './screens/ManageExpenseScreen';
+import { AllExpenses } from './screens/AllExpensesScreen';
+import { RecentExpenses } from './screens/RecentExpensesScreen';
+
+// Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const ExpensesOverview = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="RecentExpenses" component={RecentExpenses} />
+      <Tab.Screen name="AllExpenses" component={AllExpenses} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Xpense Tracker</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview}/>
+          <Stack.Screen name="ManageExpense" component={ManageExpense}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
