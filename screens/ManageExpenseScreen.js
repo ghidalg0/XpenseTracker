e68db31958c636306de5d816e522f8d5ce2dd90d
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-22 12:18:49 +0200

    added delete button conditionaly to modal 'edit'

--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -1,5 +1,11 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+// Component
+import { IconButton } from "../components/UI/IconButton";
+
+// Styling
+import { GlobalStyles } from "../constants/styles";
 
 export const ManageExpense = ({ route, navigation }) => {
 
@@ -12,8 +18,30 @@ export const ManageExpense = ({ route, navigation }) => {
     })
   }, [navigation, isEditing]);
 
+  const deleteExpenseHandler = () => {};
 
   return (
-    <Text>ManageExpense</Text>
+    <View style={styles.container}>
+      {isEditing &&
+        <View style={styles.deleteContainer}>
+            <IconButton icon="trash" color={GlobalStyles.colors.error500} size={36} onPress={deleteExpenseHandler} />
+        </View>
+      }
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  }
+});
