3f07703b07068fc8e3e52c98d93c1f9ce0f405de
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-22 12:32:03 +0200

    added buttons to the modal for confirm / cancel actions

--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -2,6 +2,7 @@ import { useLayoutEffect } from "react";
 import { StyleSheet, View } from "react-native";
 
 // Component
+import { Button } from "../components/UI/Button";
 import { IconButton } from "../components/UI/IconButton";
 
 // Styling
@@ -20,8 +21,16 @@ export const ManageExpense = ({ route, navigation }) => {
 
   const deleteExpenseHandler = () => {};
 
+  const cancelHandler = () => {};
+
+  const confirmHandler = () => {};
+
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button mode="flat" onPress={cancelHandler} style={styles.button}>Cancel</Button>
+        <Button onPress={confirmHandler} style={styles.button}>{isEditing ? "Update" : "Add"}</Button>
+      </View>
       {isEditing &&
         <View style={styles.deleteContainer}>
             <IconButton icon="trash" color={GlobalStyles.colors.error500} size={36} onPress={deleteExpenseHandler} />
@@ -36,6 +45,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
