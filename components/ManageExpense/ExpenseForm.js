95d2517fcd3a3a2d95d2fbbe30985bf907f9c6a0
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-23 10:44:01 +0200

    set the form layout

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { Input } from "./Input";
 
 export const ExpenseForm = () => {
@@ -6,16 +6,19 @@ export const ExpenseForm = () => {
   const amountChangedHandler = () => {};
 
   return (
-    <View>
-      <Input label="Amout" textInputConfig={{
-        keyboardType: 'decimal-pad',
-        onChangeText: amountChangedHandler,
-       }} />
-      <Input label="Date" textInputConfig={{
-        placeholder: "YYYY-MM-DD",
-        maxLength: 10,
-        onChangeText: null,
-       }} />
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputsRow}>
+        <Input label="Amout" style={styles.rowInput} textInputConfig={{
+          keyboardType: 'decimal-pad',
+          onChangeText: amountChangedHandler,
+        }} />
+        <Input label="Date" style={styles.rowInput} textInputConfig={{
+          placeholder: "YYYY-MM-DD",
+          maxLength: 10,
+          onChangeText: null,
+        }} />
+      </View>
       <Input label="Description" textInputConfig={{
         multiline: true,
         //autoCorrect: false, // set to true by default
@@ -23,4 +26,24 @@ export const ExpenseForm = () => {
        }} />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    marginVertical: 24,
+  },
+  inputsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,7 +1,7 @@
 import { Text, TextInput, View, StyleSheet } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 
-export const Input = ({ label, textInputConfig }) => {
+export const Input = ({ label, style, textInputConfig }) => {
 
   let inputStyles = [styles.input];
 
@@ -10,7 +10,7 @@ export const Input = ({ label, textInputConfig }) => {
   }
 
   return (
-    <View style={styles.inputContainer}>
+    <View style={[styles.inputContainer, style]}>
       <Text style={styles.label}>{label}</Text>
       <TextInput {...textInputConfig} style={inputStyles}/>
     </View>
